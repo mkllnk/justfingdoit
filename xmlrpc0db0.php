7c15ec564172716d880efd03a9c0a40832db3d72
Maikel Linke <mkllnk@web.de>
2017-03-11 14:35:45 +1100

    Static site copied via httrack from local wordpress

---
+++ xmlrpc0db0.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>http://wordpress.org/</engineLink>
+    <homePageLink>http://localhost/wp</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://localhost/wp/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://localhost/wp/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://localhost/wp/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://localhost/wp/xmlrpc.php" />
+          </apis>
+  </service>
+</rsd>
